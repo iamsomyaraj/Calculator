e912bb8a2882131008d8f43ad041916de65f76f1
iamsomyaraj <rajsomya321@gmail.com>
2025-12-09 23:09:30 +0530

    Added all code for the web calculator

---
+++ script.js
@@ -0,0 +1,23 @@
+
+const display = document.querySelector(".display input");
+
+
+function Show(input) {
+    display.value += input; 
+}
+
+
+function clearDisplay() {
+    display.value = "";
+}
+
+
+function calculate() {
+    try {
+
+        display.value = eval(display.value);
+    } 
+    catch (error) {
+        display.value = "Error";
+    }
+}
